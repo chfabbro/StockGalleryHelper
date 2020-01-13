118bfad39838a99679d6bc71b57ade48be0d955d
Christopher Smith <cfsmith@adobe.com>
2020-01-13 12:20:37 -0800

    STK-44412: Completing options dialog implementation

--- src/content-scripts/options.js
+++ src/content-scripts/options.js
@@ -31,26 +31,44 @@ $(document).ready(() => {
       API_KEY: 'apiKey',
       URL: 'endpoint',
     },
+    UI: {
+      CLOSE: '#options-close',
+    },
   };
   const formData = {
     [K.DATA.API_KEY]: {
       name: K.DATA.API_KEY,
       value: '',
-      id: '',
+      id: '#apiKeyInput',
     },
     [K.DATA.URL]: {
       name: K.DATA.URL,
       value: '',
-      id: '',
+      id: '#endpointInput',
     },
   };
 
+  // when dialog is opened, populate with current api key/url values
+  const updateUi = (apiKey, url) => {
+    const f = formData;
+    const data = K.DATA;
+    $(f[data.API_KEY].id).val(apiKey);
+    $(f[data.URL].id).val(url);
+  };
+
+  const getBaseUrl = (url) => {
+    const ex = /((https?)?:?(\/\/))?(.*)/.exec(url);
+    return (ex && ex.length > 0 && ex[4]) ? ex[4] : url;
+  };
+
   const checkAndSaveData = (formVals) => {
-    const retVals = formVals.forEach((entry) => {
+    const retVals = {};
+    formVals.forEach((entry) => {
       const { name } = entry;
       let val;
       if (name === K.DATA.URL) {
-        val = new URL(`https://${entry.value}`);
+        const { value } = entry;
+        val = `https://${getBaseUrl(value)}`;
       } else {
         const apiKeyTest = /^[a-zA-Z0-9]+$/;
         if (!apiKeyTest.test(entry.value)) {
@@ -61,31 +79,58 @@ $(document).ready(() => {
       }
       // store data locally
       formData[name].value = val;
+      // create response data
+      retVals[name] = formData[name].value;
     });
     return retVals;
   };
+
+  // closes options dialog
+  const onOptionsClose = (() => window.close());
+
+  // opens toast notification
+  const onNotification = (() => {
+    $('.toast').toast('show');
+  });
 
   const onFormSubmit = ((e) => {
     e.preventDefault();
     const $form = $(e.currentTarget);
     try {
       // validate data and save locally
-      checkAndSaveData($form.serializeArray());
+      const data = checkAndSaveData($form.serializeArray());
+      console.log(data);
       // update in chrome storage
-      // START HERE notify()
+      notify({
+        status: K.EVENTS.OPTIONS.SET,
+        data,
+      });
+      // show toast
+      onNotification();
+      // update UI with formatted values
+      updateUi(data[K.DATA.API_KEY], getBaseUrl(data[K.DATA.URL]));
     } catch (err) {
       console.error(err);
     }
-    // TODO: use notify to store send values to bg script
-    // TODO: at startup, check if values already exist and use retrieve
   });
+
   // check if values exist
   Promise.all([
     retrieve(K.DATA.API_KEY),
     retrieve(K.DATA.URL),
-  ]).then((val) => {
-    console.log(val);
+  ]).then(([apiKey, url]) => {
+    if (apiKey && url) {
+      const f = formData;
+      const data = K.DATA;
+      f[data.API_KEY].value = apiKey;
+      f[data.URL].value = url;
+      console.log(`retrieving api key and url: ${apiKey} ${url}`);
+      updateUi(apiKey, getBaseUrl(url));
+    } else {
+      console.log('api key and url not set in storage');
+    }
   });
 
   $('form').on('submit', onFormSubmit);
+  $(K.UI.CLOSE).on('click', onOptionsClose);
 });
